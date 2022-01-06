b49a2f3bca8f7c72dc867f677c74e7da738f4237
Thomas Johander <tjohander@splunk.com>
2022-01-06 12:33:54 -0300

    Corrected service name in start.sh, stop.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo systemctl start bloggerator.service
+sudo systemctl start spring-pet-clinic.service
--- stop.sh
+++ stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo systemctl stop bloggerator.service
+sudo systemctl stop spring-pet-clinic.service
