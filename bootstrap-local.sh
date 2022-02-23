96f0dcfbba8bbc3e20d5ca4df05afd73f10c8598
Thomas Johander <tjohander@splunk.com>
2022-02-23 08:17:50 -0600

    added bootstrap-local.sh, update to logback-spring.xml logging pattern

---
+++ bootstrap-local.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+export OTEL_SERVICE_NAME='my-service'
+export OTEL_RESOURCE_ATTRIBUTES='deployment.environment=tj-devlab'
+export OTEL_EXPORTER_OTLP_ENDPOINT='http://localhost:4317'
+java -javaagent:./splunk-otel-javaagent.jar -Dsplunk.metrics.enabled=true -jar build/libs/spring-petclinic-2.4.5.jar
+
