9bbce05cade75c3df8111b5e6930b10e139dec40
Bill Grant <billg@splunk.com>
2022-07-06 10:48:59 -0400

    added sleep

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -81,11 +81,11 @@ class OwnerController {
 
 	@GetMapping("/owners")
 	public String processFindForm(Owner owner, BindingResult result, Map<String, Object> model) {
-		// try {
-		// Thread.sleep(5000);
-		// }
-		// catch (InterruptedException e) {
-		// }
+		try {
+			Thread.sleep(5000);
+		}
+		catch (InterruptedException e) {
+		}
 
 		// allow parameterless GET request for /owners to return all records
 		if (owner.getLastName() == null) {
